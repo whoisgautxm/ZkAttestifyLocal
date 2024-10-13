2b191c461db2baac0507c4f3424ec8695717012a
Gautam <whoisgautxm@gmail.com>
2024-10-13 16:23:01 +0530

    added path for elf deteministic build

--- host/src/zk_proof.rs
+++ host/src/zk_proof.rs
@@ -3,8 +3,16 @@
 use crate::structs::Attest;
 use ethers_core::types::Signature;
 use ethers_core::types::{H160, H256};
-use methods::ADDRESS_ELF;
+// use methods::ADDRESS_ELF;
 use risc0_zkvm::{default_prover, ExecutorEnv, Receipt};
+use std::path::PathBuf;
+use clap::Parser;
+use std::fs; // Ensure this is present
+
+#[derive(Parser, Debug)]
+pub struct Cli {
+  path: PathBuf
+}
 
 pub fn prove_address(
     signer_address: &H160,
@@ -14,6 +22,8 @@ pub fn prove_address(
     attest: &Attest,
     domain_separator: H256,
 ) -> Receipt {
+    let args = Cli::parse();
+    let ADDRESS_ELF = args.path;
     let input: (&H160, &Signature, &u64, &u64, &Attest, H256) = (
         signer_address,
         signature,
@@ -30,5 +40,7 @@ pub fn prove_address(
         .unwrap();
 
     let prover = default_prover();
-    prover.prove(env, ADDRESS_ELF).unwrap().receipt
+    // Read the ELF file into a byte vector
+    let elf_bytes = fs::read(&ADDRESS_ELF).expect("Failed to read ELF file");
+    prover.prove(env, &elf_bytes).unwrap().receipt // Pass the byte slice
 }
